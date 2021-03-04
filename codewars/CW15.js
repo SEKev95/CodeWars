c441645b0dca98918b4044302678cd4eee6e8ffd
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-03-04 14:21:23 -0500

    Drink about

---
+++ codewars/CW15.js
@@ -0,0 +1,7 @@
+// Drink about
+function peopleWithAgeDrink(old) {
+  if (old>=21)return "drink whisky";
+  if (old<14)return"drink toddy"
+  if (old<18)return"drink coke"
+  if (old<21)return"drink beer"
+};

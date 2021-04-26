f087a0da200ee0ea565700f52ecff5ee290f9ca0
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-04-26 18:42:07 -0400

    Add files via upload

---
+++ CW51.js
@@ -0,0 +1,9 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+function square(num){
+  return num*num
+}
+
+//p- gives me an argument
+//r- the argument squared
+//e- 3^2=9
+//p- multiplication

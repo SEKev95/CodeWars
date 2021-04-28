47f125f1be0f58a9e0ba29ae25864774d9997df4
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-04-27 22:56:40 -0400

    Add files via upload

---
+++ CW52.js
@@ -0,0 +1,8 @@
+// is it even?
+function testEven(n) {
+  return n%2 === 0 
+}
+//p given a number represented by n
+//r true or false on if the number is even or not
+//e 2 = true 3 = false
+//p % remainder, === true or false

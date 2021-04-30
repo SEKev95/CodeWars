70c5e23e6dd4a158f247327281de6eeec45c6ddd
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-04-30 15:13:25 -0400

    Add files via upload

---
+++ CW55.js
@@ -0,0 +1,8 @@
+// Opposite Number
+function opposite(number) {
+ return -(number)
+}
+//p- give me a number
+//r - the opposite of that number
+//e- 1=-1
+//p- nothing special like a - multiplied will reverse it

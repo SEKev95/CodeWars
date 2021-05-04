545ee33751a5c40994cf17594308794263be3069
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-05-04 10:32:47 -0400

    Add files via upload

---
+++ CW56.js
@@ -0,0 +1,9 @@
+// Convert a string to an array
+function stringToArray(string){
+return string.split(' ')
+
+}
+//p- gives me a string
+//r- the string to an array
+//e- 'robin singh' to ['robin', 'singh']
+//p- would use .split(' ') turns strings into an array

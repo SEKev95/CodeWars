740dfac170f8307c8ab3ba38b0da15f3f05d14a8
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-05-05 19:24:20 -0400

    Add files via upload

---
+++ CW57.js
@@ -0,0 +1,8 @@
+// Return Negative
+function makeNegative(num) {
+  return -Math.abs(num);
+}
+//p- given a number
+//r- any number to negative
+//e- 1 to -1 & -5 to -5
+//p- The Math.abs() function returns the absolute value of a number.

7ebdbdf76a2d6a7f11c1b99d4bf2fc1f2ba2abb1
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-05-07 09:30:38 -0400

    Add files via upload

---
+++ CW58.js
@@ -0,0 +1,8 @@
+// String repeat
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+//p- given a string
+//r - the string repeated n times
+//e - 6,'I' to 'IIIIII'
+//p- know we need repeat() is able to repeat the string

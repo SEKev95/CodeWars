3d4524f44c71726ce79ee3640b018552e47ca453
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-05-09 21:46:51 -0400

    Opposites Attract

---
+++ CW60.js
@@ -0,0 +1,11 @@
+
+// Opposites Attract
+
+function lovefunc(flower1, flower2){
+    return flower1 % 2 !== flower2 % 2
+ }
+ 
+ //p given 2 flowers
+ //r true if either flower combo has 1 odd and 1 even
+ //e f1=4 f2=3 "they are in love" f1=2 f2=4 "they arent in love"
+ //p going to need a condition statement 

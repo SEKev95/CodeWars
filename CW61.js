fd79814b297d1a0753b61837519e0faaa932801f
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-05-10 21:26:53 -0400

    Basic Mathematical Operations

---
+++ CW61.js
@@ -0,0 +1,10 @@
+// Basic Mathematical Operations
+function basicOp(operation, value1, value2)
+{
+  return eval(value1+operation+value2)
+}
+//p value 1, value 2, operation
+//r the result of v1 and v2 with every operation
+//e basicOp('+', 4, 7)
+//p eval() is a global function in JavaScript that evaluates 
+// a specified string as JavaScript code and executes it.

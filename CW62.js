112aa8e0f552f2d61002226dd17e87755dce9b31
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-05-11 22:48:45 -0400

    Beginner - Lost Without a Map

---
+++ CW62.js
@@ -0,0 +1,9 @@
+// Beginner - Lost Without a Map
+function maps(x){
+    return x.map(n => n * 2)
+   }
+   
+   //p given an array
+   //r the nummbers in the array doubled
+   //e [1, 2, 3] --> [2, 4, 6]
+   //p told us to use the map method

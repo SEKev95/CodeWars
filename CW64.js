71dced1bca89a80c03b424ef37aa03790f8c18b3
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-05-17 22:57:54 -0400

    Small enough? - Beginner

---
+++ CW64.js
@@ -0,0 +1,9 @@
+// Small enough? - Beginner
+
+function smallEnough(a, limit){
+    return a.every(v=>v<=limit)
+   }
+   
+   //given an array and limit value
+   //r the value of the array are below or equal to the limit value , return true. else, return false
+   //e [1,2,3] value = 6 limit 3 returns false 

d5919cbde0dfbcb3c484512bfcdf03d0ac85ec90
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-05-18 21:36:41 -0400

    Sort arrays - 1

---
+++ CW65.js
@@ -0,0 +1,9 @@
+// Sort arrays - 1
+sortme = function( names ){
+    return names.sort()
+  }
+  
+  //p given an array
+  //r the array sorted
+  //e can be kev, leo, jer => jer, kev, leo
+  //p the sort method 

3b8328123061f1841a99c60d149540cbd8db0f01
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-05-21 21:36:07 -0400

    Sum of Triangular Numbers

---
+++ CW66.js
@@ -0,0 +1,19 @@
+// Sum of Triangular Numbers
+
+function sumTriangularNumbers(n) {
+    let sum=0,total=0;
+    for(let i=0;i<=n;i++){
+    sum+=i*1
+    total+=sum
+    }return total
+    }
+    
+    //P given the triangular numbers
+    //r the sum of the triangular numbers
+    //e [01]
+    //  02 [03]
+    //  04 05 [06]
+    //  07 08 09 [10]
+    //  11 12 13 14 [15]
+    //  16 17 18 19 20 [21]
+    //p for loop

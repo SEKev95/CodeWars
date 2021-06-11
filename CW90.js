7fa46417b053c160ebf6029a25dca313a6d1db83
Kevin Charles <77593772+SEKev95@users.noreply.github.com>
2021-06-10 22:06:21 -0400

    CW90.js

---
+++ CW90.js
@@ -0,0 +1,11 @@
+// Find Maximum and Minimum Values of a List
+
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
+
+//P make two functions
+//R array/vector of integers list as input and outputs, respectively,
+//E max([4,6,2,1,9,63,-134,566]) returns 566
+//  min([-52, 56, 30, 29, -54, 0, -110]) returns -110
+//  max([5]) returns 5
+//P math.min math.max
